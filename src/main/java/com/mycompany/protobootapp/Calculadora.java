87e1f4175d0afec37b2d7c3b4c21b8174a840965
vlondonoo <vlondonoo@gmail.com>
2021-03-26 19:09:41 -0500

    first

---
+++ src/main/java/com/mycompany/protobootapp/Calculadora.java
@@ -0,0 +1,12 @@
+package com.mycompany.protobootapp;
+
+public class Calculadora {
+
+    public Double cuadrado(Double numero) {
+        return numero * numero;
+    }
+    
+    public Double cubo(Double numero) {
+        return numero * numero * numero;
+    }
+}
